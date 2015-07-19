18f35e9e8fd521ffa7d6ae23d323b95219c41a20
David López <david@spextrem.com>
2015-07-19 19:15:21 +0200

    Borrar Pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -70,7 +70,7 @@ exports.create = function(req, res) {
         .then(function(){res.redirect('/quizes')}) // res.redirect: redirección HTTP a lista de preguntas
       }
     }
-  )
+  ).catch(function(error){next(error)});
 };
 
 // GET /quizes/:id/edit
@@ -96,5 +96,13 @@ exports.edit = function(req,res) {
          .then( function(){ res.redirect('/quizes');});
        }     // Redirección HTTP a lista de preguntas (URL relativo)
      }
-   );
+   ).catch(function(error){next(error)});
  };
+
+// DELETE /quizes:id
+
+exports.destroy = function(req,res) {
+  req.quiz.destroy().then(function() {
+    res.redirect('/quizes');
+  }).catch(function(error) {next(error)});
+};
