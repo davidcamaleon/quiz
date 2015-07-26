0966f45a4cfec6753d677332c736cb16efebed79
David López <david@spextrem.com>
2015-07-26 17:56:31 +0200

    Indice por Tema

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -49,7 +49,7 @@ exports.answer = function(req, res) {
 
 exports.new = function (req, res){
   var quiz = models.Quiz.build(
-    {pregunta:"Pregunta", respuesta:"Respuesta"}
+    {pregunta:"Pregunta", respuesta:"Respuesta", tema:"Tema"}
   );
   res.render('quizes/new',{quiz:quiz, errors: []});
 };
@@ -66,7 +66,7 @@ exports.create = function(req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
         quiz
-        .save({fields:["pregunta", "respuesta"]})  // guarda en DB los campos pregunta y respuesta de quiz
+        .save({fields:["pregunta", "respuesta","tema"]})  // guarda en DB los campos pregunta y respuesta de quiz
         .then(function(){res.redirect('/quizes')}) // res.redirect: redirección HTTP a lista de preguntas
       }
     }
@@ -92,7 +92,7 @@ exports.edit = function(req,res) {
          res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
        } else {
         req.quiz     // save: guarda campos pregunta y respuesta en DB
-         .save( {fields: ["pregunta", "respuesta"]})
+         .save( {fields: ["pregunta", "respuesta","tema"]})
          .then( function(){ res.redirect('/quizes');});
        }     // Redirección HTTP a lista de preguntas (URL relativo)
      }
