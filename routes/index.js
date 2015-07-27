764b01f86707b8f895b74e6af4486706d26e2873
David López <david@spextrem.com>
2015-07-27 18:52:39 +0200

    Autenticacion de Usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
-
+var sessionController = require('../controllers/session_controller');
 // Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
 //Autoload de comandos con quizId
-router.param('quizId', quizController.load);
+router.param('quizId', quizController.load); // auoload :quizId
+
+// Definicion de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario de login
+router.post('/login', sessionController.create);  // crear sesion
+router.get('/logout', sessionController.destroy); // destruir sesion
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -23,7 +27,7 @@ router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);  
+router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
 
 
 module.exports = router;
